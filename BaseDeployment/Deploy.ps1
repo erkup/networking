ceee694a422f735c11fafd3e85057dc16ee5970d
erkup <39864094+erkup@users.noreply.github.com>
2020-06-10 13:56:38 -0500

    Add task svr (#9)
    * add task server
    
    * update task RG name
    
    * fix space in VM SKU
    
    * change deploy URL to master branch
    
    Co-authored-by: erkup <erkupfer@microsoft.com>

--- BaseDeployment/Deploy.ps1
+++ BaseDeployment/Deploy.ps1
@@ -1,3 +1,3 @@
 New-AzDeployment -Location EastUS2 `
-                      -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploy.json 
+                      -TemplateUri https://raw.githubusercontent.com/erkup/networking/add-taskSvr/BaseDeployment/azuredeploy.baseDeploy.json 
                       # -TemplateParameterFile <path to the azuredeploy.network.parameters.json file> `
