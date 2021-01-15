dda0c6b5fad30968cd7a09632d477a37035a9486
jmhoffmann515 <65311237+jmhoffmann515@users.noreply.github.com>
2021-01-14 21:01:26 -0500

    Update Deploy.ps1

--- BaseDeployment/Deploy.ps1
+++ BaseDeployment/Deploy.ps1
@@ -1,3 +1,3 @@
 New-AzDeployment -Location westus `
-                      -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploy.json
+                      -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploywest.json
                       # -TemplateParameterFile <path to the azuredeploy.network.parameters.json file> `
