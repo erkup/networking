253603f1418bca99954ded140f273b70c6ab847f
erkup <eric.kupfer@gmail.com>
2021-01-22 14:22:06 -0600

    fixRdsh deployment

--- BaseDeployment/Deploy.ps1
+++ BaseDeployment/Deploy.ps1
@@ -1,3 +1,3 @@
 New-AzDeployment -Location westus `
-                      -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploywest.json
+                      -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeployjson
                       # -TemplateParameterFile <path to the azuredeploy.network.parameters.json file> `
