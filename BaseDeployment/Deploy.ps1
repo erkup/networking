54748c3a2fb92298e90e9c42c7fb910e95abc610
jmhoffmann515 <65311237+jmhoffmann515@users.noreply.github.com>
2021-06-23 17:20:28 -0400

    Update Deploy.ps1

--- BaseDeployment/Deploy.ps1
+++ BaseDeployment/Deploy.ps1
@@ -1,5 +1,5 @@
-$deployName = "Deploy" + (Get-Random)
+# $deployName = "Deploy" + (Get-Random)
 New-AzDeployment -Location EastUS
--TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploy.json 
+    -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploy.json 
 #    -TemplateFile .\azuredeploy.TempSpecDeploy.json -Name $deployName
 # -TemplateParameterFile <path to the azuredeploy.network.parameters.json file> 
