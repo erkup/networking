a51e50b6152f29df8612ac4ab81d8d13febe8858
jmhoffmann515 <65311237+jmhoffmann515@users.noreply.github.com>
2021-06-23 17:14:34 -0400

    Update Deploy.ps1

--- BaseDeployment/Deploy.ps1
+++ BaseDeployment/Deploy.ps1
@@ -1,4 +1,4 @@
 $deployName = "Deploy" + (Get-Random)
-New-AzDeployment -Location EastUS `
+New-AzDeployment -Location EastUS
     -TemplateFile .\azuredeploy.TempSpecDeploy.json -Name $deployName
 # -TemplateParameterFile <path to the azuredeploy.network.parameters.json file> 
