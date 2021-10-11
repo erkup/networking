0488a04161b9cad6980395a9106d7c6fd01c65d6
jmhoffmann515 <65311237+jmhoffmann515@users.noreply.github.com>
2021-10-11 16:03:26 -0400

    Update Deploy.ps1
    change location to uk south for focus deploy

--- BaseDeployment/Deploy.ps1
+++ BaseDeployment/Deploy.ps1
@@ -1,2 +1,2 @@
-New-AzDeployment -Location EastUS `
+New-AzDeployment -Location UKSouth `
                       -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploy.json
