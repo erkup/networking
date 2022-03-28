cd1929bb5013290aacf392b0c1981835c0612a01
jmhoffmann515 <65311237+jmhoffmann515@users.noreply.github.com>
2022-03-28 14:04:05 -0400

    Update Deploy.ps1

--- BaseDeployment/Deploy.ps1
+++ BaseDeployment/Deploy.ps1
@@ -1,2 +1,2 @@
 New-AzDeployment -Location westus `
-                      -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploy1.json
+                      -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploy.json
