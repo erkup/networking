e2195f74d512ba87bf4ab7fbf9ce4f629389d719
jmhoffmann515 <65311237+jmhoffmann515@users.noreply.github.com>
2023-09-26 14:42:13 -0400

    Update Deploy.ps1

--- BaseDeployment/Deploy.ps1
+++ BaseDeployment/Deploy.ps1
@@ -1,2 +1,2 @@
-New-AzDeployment -Location WestUS  `
+New-AzDeployment -Location EastUS  `
                       -TemplateUri https://raw.githubusercontent.com/erkup/networking/master/BaseDeployment/azuredeploy.baseDeploy.json
